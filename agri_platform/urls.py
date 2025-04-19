47292756db485543c99b65a25acc9e003212f94e
Baumendhra <baumendhra@gmail.com>
2025-04-19 09:18:59 +0530

    update urls

--- agri_platform/urls.py
+++ agri_platform/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_fromwork_simplejwt.views import( TokenObtainPairView, TokenRefreshView,)
+from rest_framework_simplejwt.views import( TokenObtainPairView, TokenRefreshView,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +25,4 @@ urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+`
